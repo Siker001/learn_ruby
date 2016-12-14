2f9caed9cbb26d0194b6e1d81be160f8c8dfd2a5
Siker001 <sikergarantalt@gmail.com>
2016-12-14 22:13:34 +0100

    Solved the first 3 challanges.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc num
+	ans = (num.to_f-32) * (5.to_f/9.to_f)
+	return ans
+end
+
+def ctof num
+	ans = (num.to_f / (5.to_f/9.to_f)) + 32
+	return ans
+end
+
+
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,47 @@
-#write your code here
+def add(par1, par2)
+	par1+par2
+end
+
+def subtract(par1, par2)
+	par1-par2
+end
+
+def sum par
+	x = 0
+	par.each do |y|
+		x = x + y
+	end
+	return x
+end
+
+def multiply par
+	x = 1
+	par.each do |y|
+		x = x * y
+	end
+	return x
+end
+
+def power(par1, par2)
+	x = 1
+	y = par1
+	while x < par2.to_i
+		y = y*par1
+		x = x+1
+	end
+	return y
+end
+
+def factorial par
+	x = par.to_i
+	if ((par == 0) || (par == 1))
+		x = 1
+	else
+		y = par.to_i
+		while y > 1
+			x = x * (y-1)
+			y = y - 1
+		end
+	return x	
+	end
+end
