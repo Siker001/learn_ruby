9f74935a7f67c9a5795e716b34030e3fc6d97e87
Siker001 <sikergarantalt@gmail.com>
2016-12-16 00:19:52 +0100

    Solution to 03 and 04 tests.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
-#write your code here
+def echo par
+	par
+end
+
+def shout par
+	par.upcase
+end
+
+def repeat(par1, par2 = 2)
+	([par1]*par2).join(' ')
+end
+
+def start_of_word(arg1, arg2)
+
+	arg1[0..arg2-1]
+end
+
+def first_word arg
+	result = arg.scan(/\w+/)
+	result[0]
+end
+
+def titleize arg
+	result = arg.scan(/\w+/)
+	x = 0
+	final = []
+	while x < result.length
+		
+		final = result[x].capitalize
+		x = x +1
+	end
+	if x > 1
+		final.split(/ |\_/).map(&:capitalize).join(" ")
+
+	end
+	return final
+	
+	
+	
+	
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,59 @@
-#write your code here
+def translate arg
+	# if there is " " then run the word separator and deal with it like an array
+	firstLetter = arg[0].to_s
+	secondLetter = arg[1].to_s
+	thirdLetter = arg[2].to_s
+	if arg.include? " "
+		result = arg.scan(/\w+/)
+	# when array - check each element aka words one by one and after modification push them back into one string somehow
+		n = 0
+		moreWords = []
+		while n < result.length.to_i
+			firstLetter = result[n][0].to_s
+			secondLetter = result[n][1].to_s
+			thirdLetter = result[n][2].to_s
+			if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u')
+				answer = result[n].to_s + "ay"
+			elsif (firstLetter == "q" && secondLetter == "u")
+				answer = result[n][2..-1].to_s + firstLetter + secondLetter + "ay"
+			elsif (secondLetter == "q" && thirdLetter == "u")
+				answer = result[n][3..-1].to_s + firstLetter + secondLetter + thirdLetter + "ay"
+			else
+				if (secondLetter != 'a' && secondLetter != 'e' && secondLetter != 'i' && secondLetter != 'o' && secondLetter != 'u')
+					if (thirdLetter != 'a' && thirdLetter != 'e' && thirdLetter != 'i' && thirdLetter != 'o' && thirdLetter != 'u')
+						answer = result[n][3..-1].to_s + firstLetter + secondLetter + thirdLetter + "ay"
+					else
+						answer = result[n][2..-1].to_s + firstLetter + secondLetter + "ay"
+					end
+				else	
+					answer = (result[n].to_s.delete firstLetter) + firstLetter + "ay"	
+				end
+			end
+			moreWords.push answer
+			n = n + 1
+		end
+		moreWords.join(" ")
+	# if there's no " " in argument, then jump to the regular dealing with words 
+	else
+		if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u')
+			answer = arg.to_s + "ay"
+		elsif (firstLetter == "q" && secondLetter == "u")
+			answer = arg[2..-1].to_s + firstLetter + secondLetter + "ay"
+		elsif (secondLetter == "q" && thirdLetter == "u")
+			answer = arg[3..-1].to_s + firstLetter + secondLetter + thirdLetter + "ay"
+		else
+			if (secondLetter != 'a' && secondLetter != 'e' && secondLetter != 'i' && secondLetter != 'o' && secondLetter != 'u')
+				if (thirdLetter != 'a' && thirdLetter != 'e' && thirdLetter != 'i' && thirdLetter != 'o' && thirdLetter != 'u')
+					answer = arg[3..-1].to_s + firstLetter + secondLetter + thirdLetter + "ay"
+				else
+					answer = arg[2..-1].to_s + firstLetter + secondLetter + "ay"
+				end
+			else
+				answer = (arg.to_s.delete firstLetter) + firstLetter + "ay"	
+			end
+		end
+	end
+end
+
+
+puts translate "eat banana"
