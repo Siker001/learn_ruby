9089a05b118e19fd6d4264c92b35e2ac572084c6
Siker001 <sikergarantalt@gmail.com>
2016-12-18 17:01:51 +0100

    Added solution to 05, 06 exercises.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title
+		words = @title.split(" ")
+		
+		exceptions = ["the", "a", "an", "and", "in", "of", "on", "at"]
+		words.each do |x| 
+			x.capitalize! unless exceptions.include? x ;
+		end
+		
+		words[0].capitalize!
+		result = words.join(" ")
+		
+	end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string 
+  	instr = Time.at(@seconds)-3600
+  	sec = instr.strftime("%H:%M:%S")
+  end
+
 end
+
